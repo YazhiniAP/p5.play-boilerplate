5f269b2786a961bbfd21f448490811c0dead824f
YazhiniAP <60008636+YazhiniAP@users.noreply.github.com>
2020-01-18 10:49:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,14 @@
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+ // createSprite(400, 200, 50, 50);
+  fill("yellow");
+  ellipse(400,200,30,25);
+    fill("blue");
+  ellipse(200,200,20,15);
+    fill("red");
+  ellipse(200,100,20,15);
+    fill("green");
+  ellipse(600,100,20,15);
 }
 
 function draw() {
